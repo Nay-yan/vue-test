590c1cf00ae0a8981bded0c37d405d6cb4170d14
WaiYan <aa8184792@gmial.com>
2021-04-30 00:26:13 +0630

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+    <h4 class="text-primary mb-4">Hello Components Template</h4> 
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name:"HelloWorld"
+  }
+</script>
+
+<style scoped>
+
+</style>

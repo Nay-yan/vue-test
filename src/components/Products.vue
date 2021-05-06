d3cbacde10429d9b6db058a9d767517e38e799ad
WaiYan <aa8184792@gmial.com>
2021-05-06 14:51:52 +0630

    design

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,19 +3,21 @@
       <Master>
          <Nav/> 
 
+        <div class="container prodSection mb-2">
+          <p class="totle">Cart Totle - {{ products.length }}</p>
+        </div>
          <!-- Loader -->
-        <Loader v-show="isLoad"/>
+         <Loader v-show="isLoad"/>
 
-        <div v-show="!isLoad" class="card-columns container prodSection">
-      
-            <div v-for="(p,i) in products" :key="i" class="card border-success" style="width: 21rem;">
-              <div class="card-header cardTitle">{{p.title}}</div>
+        <div v-show="!isLoad" class="card-columns container ">
+            <div v-for="(p,i) in products" :key="i" class="card border-0 shadow" style="width: 21rem;">
                 <div class="card-body p-0 text-center">
                   <img :src="p.image" class="card-img-top rounded-0 img ">
                 </div>
-                <div class="card-footer text-primary cardFooter">
+                <div class="card-footer text-primary cardFooter pb-4">
+                  <p class="cardTitle">{{p.title}}</p>
                   Price- ${{p.price}}
-                  <button class="btn btn-outline-info btn-sm float-right" @click="addToCart()">Add To Cart</button>
+                  <button class="btn btn-outline-primary btn-sm float-right font-weight-bold" @click="addToCart()">Add To Cart</button>
                 </div>
             </div>
           </div>
@@ -66,12 +68,21 @@ import axios from 'axios';
 
 }
 .cardTitle{
-  background-color: rgba(156, 255, 156, 0.904);
+  font-weight: normal;
+  color: rgb(117, 117, 117);
+  text-align: center;
 }
 .cardFooter{
-  background-color: rgba(214, 255, 214, 0.884);
+  background-color: rgb(226, 243, 252);
+  font-weight: bold;
 }
 .prodSection{
   padding-top: 80px;
+}
+.totle{
+  background-color: rgb(2, 80, 116);
+  color: white;
+  padding: 10px 10px;
+  border-radius: 5px;
 }
 </style>
